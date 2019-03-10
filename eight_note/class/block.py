d2e7ce881f9e820d19afa8d0b5ea6ac42e4eb6ff
WatsonJay <446725026@qq.com>
2019-03-10 23:40:15 +0800

    update

--- eight_note/class/block.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-# 定义block类，用于随机生成地面块
-import random
-import cocos
-import os
--- eight_note/classes/eight_note.py
+++ eight_note/classes/eight_note.py
@@ -3,9 +3,9 @@
 import cocos
 import os
 
-class music_boy(cocos.sprite.Sprite):
+class eight_note(cocos.sprite.Sprite):
     def __init__(self):
-        super(music_boy,self).__init__('resources/eight_note.png')
+        super(eight_note,self).__init__('resources/eight_note.png')
         # 是否可以跳跃
         self.can_jump = False
         # 速度
@@ -15,14 +15,14 @@ class music_boy(cocos.sprite.Sprite):
         #音符酱的位置
         self.position = 80, 280
         self.scheduled(self.update)
-    #声控跳跃
-    def jump(self, h):
-
-    #重力下落
-    def fall(self, y):
-
-    #着陆静止
-    def stay(self, dt):
-
-    #重置
-    def reset(self):
+    # #声控跳跃
+    # def jump(self, h):
+    #
+    # #重力下落
+    # def fall(self, y):
+    #
+    # #着陆静止
+    # def stay(self, dt):
+    #
+    # #重置
+    # def reset(self):
